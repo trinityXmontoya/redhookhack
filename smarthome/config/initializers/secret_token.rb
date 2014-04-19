9a856695a1a04536896ee551ba913c948fd5a100
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-18 20:40:35 -0400

    first commit, rails initialize files

---
+++ smarthome/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smarthome::Application.config.secret_key_base = 'fc141d7e387d4ced8946b04cb862c42f39fa2d6d3967501c0ef8fead39063033ab44749b14af776f0e7ce5f964b589ba8e18e1a2e36f40e5fe784410eecc7198'
