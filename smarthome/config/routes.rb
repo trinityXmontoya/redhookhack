dc8e465a73e89ea125af173b3a78ef066a26f870
trinityXmontoya <trinityXmontoya@gmail.com>
2014-04-18 21:54:08 -0400

    add home_systems index to route

--- smarthome/config/routes.rb
+++ smarthome/config/routes.rb
@@ -14,6 +14,8 @@ Smarthome::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :home_systems, only: ['index']
+
   # Example resource route with options:
   #   resources :products do
   #     member do
